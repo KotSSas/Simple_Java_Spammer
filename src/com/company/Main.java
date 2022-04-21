5d736c39ce790896a64d5ac5e6146e308727d1fe
Smug <sashasmug@gmail.com>
2022-04-21 11:16:14 +0300

    Some changes to script

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,28 +9,35 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) throws AWTException, InterruptedException {
-        // write your code here
-        int x = 10;
-        Robot robot = new Robot();
-        Scanner sc = new Scanner(System.in);
+        int x = 1;
 
-        System.out.println("Enter the message:");
-        String text = sc.nextLine();
-        StringSelection stringSelection = new StringSelection(text);
+        Robot robot = new Robot();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter your text:");
+        String Text = scanner.nextLine();
+
+        StringSelection stringSelection = new StringSelection(Text);
         Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
         clipboard.setContents(stringSelection, null);
 
         System.out.println("The spamming will start in 5 seconds");
-        Thread.sleep(5000); //wait for 5000 mls
-        while (x<=10) {
-            Thread.sleep(100); //wait for 5000 mls
+        Thread.sleep(5000);
+
+        while(x <= 100) {
+
+            Thread.sleep(100);
+
             robot.keyPress(KeyEvent.VK_CONTROL);
-            robot.keyRelease(KeyEvent.VK_CONTROL);
             robot.keyPress(KeyEvent.VK_V);
+            robot.keyRelease(KeyEvent.VK_CONTROL);
             robot.keyRelease(KeyEvent.VK_V);
 
             robot.keyPress(KeyEvent.VK_ENTER);
             robot.keyRelease(KeyEvent.VK_ENTER);
+
+            x++;
+
         }
 
     }
